6ddff91f0e0b36329b9ca49a9f5bd07fb96b894b
JMax45 <maxmailru.45@gmail.com>
2020-09-23 20:43:48 +0200

    Added ytdl module

--- src/commands/start.js
+++ src/commands/start.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Receive a greeting from the bot',
     public: true, 
     execute(ctx){
-        ctx.replyWithMarkdown('Welcome 👋.                            Thanks to this bot you can simply download your favorite songs and listen to them whenever you want, even offline. World');
+        ctx.replyWithMarkdown('Welcome 👋.\nThanks to this bot you can simply download your favorite songs and listen to them whenever you want, even offline. World');
     }
 }
