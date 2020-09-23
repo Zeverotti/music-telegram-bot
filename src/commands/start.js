96196be69f02fa8621949ca46f78275a63044e8c
Zeverotti <zeverotti@gmail.com>
2020-09-23 20:12:17 +0200

    Added welcome message

--- src/commands/start.js
+++ src/commands/start.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Receive a greeting from the bot',
     public: true, 
     execute(ctx){
-        ctx.replyWithMarkdown('Hello World');
+        ctx.replyWithMarkdown('Welcome.                            Thanks to this bot you can simply download your favorite songs and listen to them whenever you want, even offline. World');
     }
 }
