43a396dde5cf7dc0fa2eb5439496bd7c20b58064
Zeverotti <zeverotti@gmail.com>
2020-09-23 22:32:56 +0200

    Added \n help

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Receive help from the bot',
     public: true, 
     execute(ctx){
-        ctx.replyWithMarkdown('How to use JZM: \n\n/ytd link \n_Download the audio from the video_ \n/music \n_See downloaded music_');
+        ctx.replyWithMarkdown('How to use JZM: \n\n/ytd link \n_Download the audio from the video_ \n\n/music \n_See downloaded music_');
     }
 }
