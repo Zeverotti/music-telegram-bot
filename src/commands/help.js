918f030b04243df8c3648cc8863498601b5b0739
Zeverotti <zeverotti@gmail.com>
2020-09-23 22:16:17 +0200

    Fixed help #3

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Receive help from the bot',
     public: true, 
     execute(ctx){
-        ctx.replyWithMarkdown('How to use JZM: \nList of commands: \n/ytd link for download the audio from the video \n/music for see downloaded music');
+        ctx.replyWithMarkdown('How to use JZM: \n/ytd link for download the audio from the video \n/music for see downloaded music');
     }
 }
